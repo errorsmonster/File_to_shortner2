ed3f28bc0e7ba710272e2120f5065616fbb2684e
HMTD-Links <109021412+HMTD-Links@users.noreply.github.com>
2023-05-15 17:55:30 +0530

    Update stream.py

--- WebStreamer/bot/plugins/stream.py
+++ WebStreamer/bot/plugins/stream.py
@@ -11,6 +11,7 @@ from pyrogram.enums.parse_mode import ParseMode
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram.enums.parse_mode import ParseMode
 from WebStreamer.utils.human_readable import humanbytes
+from WebStreamer.utils.file_properties import get_name, get_media_file_size
 
 @StreamBot.on_message(
     filters.private
