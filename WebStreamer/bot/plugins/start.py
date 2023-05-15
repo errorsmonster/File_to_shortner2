4a485cd33d2bc2b1220a4b71e1839617cc045805
HMTD-Links <109021412+HMTD-Links@users.noreply.github.com>
2023-05-15 17:01:08 +0530

    Update start.py

--- WebStreamer/bot/plugins/start.py
+++ WebStreamer/bot/plugins/start.py
@@ -5,6 +5,7 @@ from pyrogram import filters, enums
 from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup, CallbackQuery
 from WebStreamer.vars import Var 
 from WebStreamer.bot import StreamBot
+from pyrogram.enums.parse_mode import ParseMode
 OWNER_ID = Var.OWNER_ID
 
 STAR_BUTTONS = [
@@ -36,3 +37,43 @@ async def start(_, m: Message):
             reply_markup=reply_markup,
             disable_web_page_preview=True
         )
+            
+@StreamBot.on_message(filters.command(["help"]) & filters.private & filters.incoming)
+async def help(client, message):
+    reply_markup = InlineKeyboardMarkup(STAR_BUTTONS)
+    mention = message.from_user.mention
+    if Var.ALLOWED_USERS and not ((str(message.from_user.id) in Var.ALLOWED_USERS) or (message.from_user.username in Var.ALLOWED_USERS)):
+        return await message.reply(
+            "<b>You are not in the allowed list of users who can use me. \
+            Check <a href='https://github.com/EverythingSuckz/TG-FileStreamBot#optional-vars'>this link</a> for more info.</b>",
+            disable_web_page_preview=True, quote=True
+        )
+    await message.reply_text(
+            text="<b>Hi 👋🏻 {} ♥️,  Send me a File 📂 to get an Instant Stream link.</b>".format(
+                mention
+            ),
+            quote=True,
+            parse_mode=ParseMode.HTML,
+            reply_markup=reply_markup,
+            disable_web_page_preview=True
+        )
+
+@StreamBot.on_message(filters.command(["about"]) & filters.private & filters.incoming)
+async def about(client, message):
+    mention = message.from_user.mention
+    reply_markup = InlineKeyboardMarkup(STAR_BUTTONS)
+    if Var.ALLOWED_USERS and not ((str(message.from_user.id) in Var.ALLOWED_USERS) or (message.from_user.username in Var.ALLOWED_USERS)):
+        return await message.reply(
+            "<b>You are not in the allowed list of users who can use me. \
+            Check <a href='https://github.com/EverythingSuckz/TG-FileStreamBot#optional-vars'>this link</a> for more info.</b>",
+            disable_web_page_preview=True, quote=True
+        )
+    await message.reply_text(
+            text="<b>Hi 👋🏻 {} ♥️,  Send me a File 📂 to get an Instant Stream link.</b>".format(
+                mention
+            ),
+            quote=True,
+            parse_mode=ParseMode.HTML,
+            reply_markup=reply_markup,
+            disable_web_page_preview=True
+        )            
