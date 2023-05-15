ccb232675e5e3ee46651e57ab3ce08414e1e0017
HMTD-Links <109021412+HMTD-Links@users.noreply.github.com>
2023-05-15 19:35:00 +0530

    Update admin.py

--- WebStreamer/bot/plugins/admin.py
+++ WebStreamer/bot/plugins/admin.py
@@ -9,7 +9,8 @@ from WebStreamer.utils.broadcast import send_msg
 from WebStreamer.utils.database import Database
 from WebStreamer.bot import StreamBot
 from WebStreamer.vars import Var
-from pyrogram import filters, Client
+from pyrogram import filters, Client, enums
+from pyrogram.enums.parse_mode import ParseMode
 from pyrogram.types import Message
 db = Database(Var.DATABASE_URL, Var.SESSION_NAME)
 broadcast_ids = {}
@@ -18,7 +19,7 @@ broadcast_ids = {}
 @StreamBot.on_message(filters.command("stats") & filters.private & filters.user(Var.OWNER_ID))
 async def sts(c: Client, m: Message):
     total_users = await db.total_users_count()
-    await m.reply_text(text=f"**Total Users in DB:** `{total_users}`", parse_mode="Markdown", quote=True)
+    await m.reply_text(text=f"**Total Users in DB:** `{total_users}`", parse_mode=ParseMode.HTML, quote=True)
 
 
 @StreamBot.on_message(filters.command("broadcast") & filters.private & filters.user(Var.OWNER_ID) & filters.reply)
