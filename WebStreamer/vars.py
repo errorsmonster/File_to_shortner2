288df7b044f39e766ca9ca0bf963fc113bcdd64b
HMTD-Links <109021412+HMTD-Links@users.noreply.github.com>
2023-05-16 09:18:05 +0530

    Update vars.py

--- WebStreamer/vars.py
+++ WebStreamer/vars.py
@@ -31,16 +31,13 @@ class Var(object):
         sys.exit("Hash length should be greater than 5 and less than 64")
     FQDN = str(environ.get("FQDN", BIND_ADDRESS))
     URL = "https://{}{}/".format(
-            "s" if HAS_SSL else "", FQDN, "" if NO_PORT else ":" + str(PORT)
+            FQDN, "" if NO_PORT else ":" + str(PORT)
         )
     KEEP_ALIVE = str(environ.get("KEEP_ALIVE", "0").lower()) in  ("1", "true", "t", "yes", "y")
     DEBUG = str(environ.get("DEBUG", "0").lower()) in ("1", "true", "t", "yes", "y")
     USE_SESSION_FILE = str(environ.get("USE_SESSION_FILE", "0").lower()) in ("1", "true", "t", "yes", "y")
     ALLOWED_USERS = [x.strip("@ ") for x in str(environ.get("ALLOWED_USERS", "") or "").split(",") if x.strip("@ ")]
-    DB_URL = environ.get('DATABASE_URI', "mongodb+srv://KarthikMovies:KarthikUK007@cluster0.4l5byki.mongodb.net/?retryWrites=true&w=majority")
-    DB_NAME = environ.get('DATABASE_NAME', "Cluster0")
     LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001342411240"))
-    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "1391556668").split())
     BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
     DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://KarthikMovies:KarthikUK007@cluster0.4l5byki.mongodb.net/?retryWrites=true&w=majority'))
     UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', "Star_Bots_Tamil"))
