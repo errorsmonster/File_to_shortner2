fc4a9f6a36c83087d9bc3b274eb36d78ed46add3
HMTD-Links <109021412+HMTD-Links@users.noreply.github.com>
2023-05-16 18:28:42 +0530

    Update start.py

--- WebStreamer/bot/plugins/start.py
+++ WebStreamer/bot/plugins/start.py
@@ -86,29 +86,25 @@ ABOUT_BUTTONS = InlineKeyboardMarkup(
         InlineKeyboardButton('🏠 Home', callback_data='home')
         ]]
     )
-
 @StreamBot.on_callback_query()
 async def cb_data(bot, update):
     if update.data == "home":
         await update.message.edit_text(
             text=START_TEXT.format(update.from_user.mention),
             disable_web_page_preview=True,
-            reply_markup=START_BUTTONS,
-            quote=True    
+            reply_markup=START_BUTTONS
         )
     elif update.data == "help":
         await update.message.edit_text(
             text=HELP_TEXT,
             disable_web_page_preview=True,
-            reply_markup=HELP_BUTTONS,
-            quote=True    
+            reply_markup=HELP_BUTTONS
         )
     elif update.data == "about":
         await update.message.edit_text(
             text=ABOUT_TEXT,
             disable_web_page_preview=True,
-            reply_markup=ABOUT_BUTTONS,
-            quote=True    
+            reply_markup=ABOUT_BUTTONS
         )
     else:
         await update.message.delete()
@@ -129,13 +125,13 @@ def get_media_file_name(m):
         return None
 
 
-@StreamBot.on_message(filters.command('start') & filters.private)
+@StreamBot.on_message(filters.command('start') & filters.private & ~filters.edited)
 async def start(b, m):
     if not await db.is_user_exist(m.from_user.id):
         await db.add_user(m.from_user.id)
         await b.send_message(
-            Var.LOG_CHANNEL,
-            f"<b>#New_User_Joined\n\n᚛›Name :- <a href=tg://user?id={m.from_user.id}>{m.from_user.first_name}</a>\n᚛› ID :- <code>{m.from_user.id}</code>\n᚛› From Bot :- <a href=https://t.me/File_to_Link_Star_Bot>File to Link Star Bots</a></b>", parse_mode=ParseMode.HTML
+            Var.BIN_CHANNEL,
+            f"**Nᴇᴡ Usᴇʀ Jᴏɪɴᴇᴅ:** \n\n__Mʏ Nᴇᴡ Fʀɪᴇɴᴅ__ [{m.from_user.first_name}](tg://user?id={m.from_user.id}) __Sᴛᴀʀᴛᴇᴅ Yᴏᴜʀ Bᴏᴛ !!__"
         )
     usr_cmd = m.text.split("_")[-1]
     if usr_cmd == "/start":
@@ -145,38 +141,35 @@ async def start(b, m):
                 if user.status == "kicked":
                     await b.send_message(
                         chat_id=m.chat.id,
-                        text="<b>Sorry <a href='tg://user?id={m.from_user.id}>{m..first_name}</a>,\nYou're Banned 🚫 To Use Me❓.\n\n Contact Developer <a href='https://t.me/Star_Bots_Tamil_Support'>Star Bots Tamil Support</a> They will Help You.</b>",
-                        parse_mode=ParseMode.HTML,
-                        disable_web_page_preview=True,
-                        quote=True    
+                        text="__Sᴏʀʀʏ Sɪʀ, Yᴏᴜ ᴀʀᴇ Bᴀɴɴᴇᴅ ᴛᴏ ᴜsᴇ ᴍᴇ. Cᴏɴᴛᴀᴄᴛ ᴛʜᴇ Dᴇᴠᴇʟᴏᴘᴇʀ__\n\n @greymatters_bots_discussion **Tʜᴇʏ Wɪʟʟ Hᴇʟᴘ Yᴏᴜ**",
+                        parse_mode="markdown",
+                        disable_web_page_preview=True
                     )
                     return
             except UserNotParticipant:
                 await b.send_message(
                     chat_id=m.chat.id,
-                    text="<b>Please Join Our Updates Channel to Use Me❗\n\nDue To Overload, Only Channel Subscribers Can Use to Me❗.</b>", quote=True,
+                    text="<i>Jᴏɪɴ ᴍʏ ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ ᴛᴏ ᴜsᴇ ᴍᴇ 🔐</i>",
                     reply_markup=InlineKeyboardMarkup(
                         [[
-                            InlineKeyboardButton("🤖 Join Our Bot Channel", url=f"https://t.me/{Var.UPDATES_CHANNEL}")
+                            InlineKeyboardButton("Jᴏɪɴ ɴᴏᴡ 🔓", url=f"https://t.me/{Var.UPDATES_CHANNEL}")
                             ]]
                     ),
-                    parse_mode=ParseMode.HTML
+                    parse_mode="HTML"
                 )
                 return
             except Exception:
                 await b.send_message(
                     chat_id=m.chat.id,
-                    text="<b>Something Wrong❗\nYou're Not Added Admin to Update Channel.\n\n👥 Support :- <a href=https://t.me/Star_Bots_Tamil_Support><b>Star Bots Tamil Support</b></a></b>",
-                    parse_mode=ParseMode.HTML,
-                    disable_web_page_preview=True,
-                    quote=True)
+                    text="<i>Sᴏᴍᴇᴛʜɪɴɢ ᴡʀᴏɴɢ ᴄᴏɴᴛᴀᴄᴛ ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ</i> <b><a href='https://t.me/greymatters_bots_discussion'>[ ᴄʟɪᴄᴋ ʜᴇʀᴇ ]</a></b>",
+                    parse_mode="HTML",
+                    disable_web_page_preview=True)
                 return
         await m.reply_text(
             text=START_TEXT.format(m.from_user.mention),
-            parse_mode=ParseMode.HTML,
+            parse_mode="HTML",
             disable_web_page_preview=True,
-            reply_markup=START_BUTTONS,
-            quote=True    
+            reply_markup=START_BUTTONS
               )                                                                         
                                                                                        
                                                                             
@@ -187,82 +180,76 @@ async def start(b, m):
                 if user.status == "kicked":
                     await b.send_message(
                         chat_id=m.chat.id,
-                        text="<b>Sorry <a href='tg://user?id={m.from_user.id}>{m..first_name}</a>,\nYou're Banned 🚫 To Use Me❓.\n\n Contact Developer <a href='https://t.me/Star_Bots_Tamil_Support'>Star Bots Tamil Support</a> They will Help You.</b>",
-                        parse_mode=ParseMode.HTML,
+                        text="**Sᴏʀʀʏ Sɪʀ, Yᴏᴜ ᴀʀᴇ Bᴀɴɴᴇᴅ ᴛᴏ ᴜsᴇ ᴍᴇ. Qᴜɪᴄᴋʟʏ ᴄᴏɴᴛᴀᴄᴛ** @greymatters_bots_discussion",
+                        parse_mode="markdown",
                         disable_web_page_preview=True
                     )
                     return
             except UserNotParticipant:
                 await b.send_message(
                     chat_id=m.chat.id,
-                    text="<b>Please Join Our Updates Channel to Use Me❗\n\nDue To Overload, Only Channel Subscribers Can Use to Me❗.</b>",
+                    text="**Pʟᴇᴀsᴇ Jᴏɪɴ Mʏ Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ ᴛᴏ ᴜsᴇ ᴛʜɪs Bᴏᴛ**!\n\n**Dᴜᴇ ᴛᴏ Oᴠᴇʀʟᴏᴀᴅ, Oɴʟʏ Cʜᴀɴɴᴇʟ Sᴜʙsᴄʀɪʙᴇʀs ᴄᴀɴ ᴜsᴇ ᴛʜᴇ Bᴏᴛ**!",
                     reply_markup=InlineKeyboardMarkup(
                         [[
-                          InlineKeyboardButton("🤖 Join Our Bot Channel", url=f"https://t.me/{Var.UPDATES_CHANNEL}")],
-                         [InlineKeyboardButton("🔄 Refresh / Try Again", url=f"https://t.me/{(await b.get_me()).username}?start=Star_Bots_Tamil_{usr_cmd}")
+                          InlineKeyboardButton("🤖 Jᴏɪɴ Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ", url=f"https://t.me/{Var.UPDATES_CHANNEL}")],
+                         [InlineKeyboardButton("🔄 Refresh / Try Again", url=f"https://t.me/{(await b.get_me()).username}?start=Moksh_b658_{usr_cmd}")
                         
                         ]]
                     ),
-                    parse_mode=ParseMode.HTML
+                    parse_mode="markdown"
                 )
                 return
             except Exception:
                 await b.send_message(
                     chat_id=m.chat.id,
-                    text="<b>Something Wrong❗\nYou're Not Added Admin to Update Channel.\n\n👥 Support :- <a href=https://t.me/Star_Bots_Tamil_Support><b>Star Bots Tamil Support</b></a></b>",
-                    parse_mode=ParseMode.HTML,
-                    disable_web_page_preview=True,
-                    quote=True)
+                    text="**Sᴏᴍᴇᴛʜɪɴɢ ᴡᴇɴᴛ Wʀᴏɴɢ. Cᴏɴᴛᴀᴄᴛ ᴍᴇ** [GreyMatter Support](https://t.me/greymatters_bots_discussion).",
+                    parse_mode="markdown",
+                    disable_web_page_preview=True)
                 return
 
         get_msg = await b.get_messages(chat_id=Var.BIN_CHANNEL, message_ids=int(usr_cmd))
-        file_hash = get_hash(log_msg, Var.HASH_LENGTH)
         file_name = get_media_file_name(get_msg)
         file_size = humanbytes(get_media_file_size(get_msg))
-        file_caption = m.caption
-        stream_link = "https://{}:{}/{}/{}".format(Var.FQDN, Var.PORT, log_msg.id, file_name)
-        watch_link = "https://{}:{}/Watch/{}/{}".format(Var.FQDN, Var.PORT, log_msg.id, file_name)
-        short_link = "https://{}:{}/{}/{}".format(Var.FQDN, Var.PORT, file_hash, log_msg.id)
-        shorten_urls = await short(stream_link)
-        
-        msg_text ="""
-<b><i>Your Link is Generated... ⚡</i>\n
-📁 File Name :- {}\n
-📦 File Size :- {}\n
-🔠 File Captain :- {}\n
-📥 Download Link :- {}\n
-🖥 Watch Link :- {}\n
-🔗 Shortened Link :- {}\n
-{}\n
-❗ Note :- This Link is Permanent and Won't Gets Expired 🚫\n
-©️ <a href=https://t.me/Star_Bots_Tamil><b></b>Star Bots Tamil</a></b></b>"""
 
+        stream_link = "https://{}/{}/{}".format(Var.FQDN, get_msg.message_id, file_name) if Var.ON_HEROKU or Var.NO_PORT else \
+            "http://{}:{}/{}/{}".format(Var.FQDN,
+                                     Var.PORT,
+                                     get_msg.message_id,
+                                     file_name)
+
+        msg_text ="""
+<i><u>𝗬𝗼𝘂𝗿 𝗟𝗶𝗻𝗸 𝗚𝗲𝗻𝗲𝗿𝗮𝘁𝗲𝗱 !</u></i>\n
+<b>📂 Fɪʟᴇ ɴᴀᴍᴇ :</b> <i>{}</i>\n
+<b>📦 Fɪʟᴇ ꜱɪᴢᴇ :</b> <i>{}</i>\n
+<b>📥 Dᴏᴡɴʟᴏᴀᴅ :</b> <i>{}</i>\n
+<b>🚸 Nᴏᴛᴇ : Lɪɴᴋ ᴇxᴘɪʀᴇᴅ ɪɴ 24 ʜᴏᴜʀꜱ</b>\n
+<i>🍃 Bᴏᴛ Mᴀɪɴᴛᴀɪɴᴇᴅ Bʏ :</i> <b>@GreyMatter_bots</b>
+"""
 
         await m.reply_text(
-            text=msg_text.format(file_name, file_size, file_caption, stream_link, watch_link, short_link, shorten_urls),
-            parse_mode=ParseMode.HTML, quote=True,
-            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("📥 Download Link", url=stream_link)], [InlineKeyboardButton("🖥 Watch Link", url=watch_link)], [InlineKeyboardButton("🔗 Shortened Link", url=short_link)], [InlineKeyboardButton("🔥 Update Channel", url="https://t.me/Star_Bots_Tamil")]])
+            text=msg_text.format(file_name, file_size, stream_link),
+            parse_mode="HTML",
+            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Dᴏᴡɴʟᴏᴀᴅ ɴᴏᴡ 📥", url=stream_link)]])
         )
 
 
 
 @StreamBot.on_message(filters.private & filters.command(["about"]))
-async def start(client, message):
-    await message.reply_text(
-        text=ABOUT_TEXT.format(message.from_user.mention),
+async def start(bot, update):
+    await update.reply_text(
+        text=ABOUT_TEXT.format(update.from_user.mention),
         disable_web_page_preview=True,
-        reply_markup=ABOUT_BUTTONS,
-        quote=True    
+        reply_markup=ABOUT_BUTTONS
     )
 
 
-@StreamBot.on_message(filters.command('help') & filters.private)
+@StreamBot.on_message(filters.command('help') & filters.private & ~filters.edited)
 async def help_handler(bot, message):
     if not await db.is_user_exist(message.from_user.id):
         await db.add_user(message.from_user.id)
         await bot.send_message(
-            Var.LOG_CHANNEL,
-            f"<b>#New_User_Joined\n\n᚛›Name :- <a href='tg://user?id={m.from_user.id}'>{m.from_user.first_name}</a>\n᚛› ID :- <code>{m.from_user.id}</code>\n᚛› From Bot :- <a href=https://t.me/File_to_Link_Star_Bot><b>File to Link Star Bots</b></a></b>"
+            Var.BIN_CHANNEL,
+            f"**Nᴇᴡ Usᴇʀ Jᴏɪɴᴇᴅ **\n\n__Mʏ Nᴇᴡ Fʀɪᴇɴᴅ__ [{message.from_user.first_name}](tg://user?id={message.from_user.id}) __Started Your Bot !!__"
         )
     if Var.UPDATES_CHANNEL is not None:
         try:
@@ -270,199 +257,36 @@ async def help_handler(bot, message):
             if user.status == "kicked":
                 await bot.send_message(
                     chat_id=message.chat.id,
-                    text="<b>Sorry <a href='tg://user?id={m.from_user.id}>{m.from_user.first_name}</a>,\nYou're Banned 🚫 To Use Me❓.\n\n Contact Developer <a href='https://t.me/Star_Bots_Tamil_Support'>Star Bots Tamil Support</a> They will Help You.</b>",
-                    parse_mode=ParseMode.HTML,
-                    disable_web_page_preview=True,
-                    quote=True
+                    text="<i>Sᴏʀʀʏ Sɪʀ, Yᴏᴜ ᴀʀᴇ Bᴀɴɴᴇᴅ ᴛᴏ ᴜsᴇ ᴍᴇ. Cᴏɴᴛᴀᴄᴛ ᴛʜᴇ Dᴇᴠᴇʟᴏᴘᴇʀ</i>",
+                    parse_mode="HTML",
+                    disable_web_page_preview=True
                 )
                 return
         except UserNotParticipant:
             await bot.send_message(
                 chat_id=message.chat.id,
-                text="<b>Please Join Our Updates Channel to Use Me❗\n\nDue To Overload, Only Channel Subscribers Can Use to Me❗.</b>",
+                text="**Pʟᴇᴀsᴇ Jᴏɪɴ Mʏ Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ ᴛᴏ ᴜsᴇ ᴛʜɪs Bᴏᴛ!**\n\n__Dᴜᴇ ᴛᴏ Oᴠᴇʀʟᴏᴀᴅ, Oɴʟʏ Cʜᴀɴɴᴇʟ Sᴜʙsᴄʀɪʙᴇʀs ᴄᴀɴ ᴜsᴇ ᴛʜᴇ Bᴏᴛ!__",
                 reply_markup=InlineKeyboardMarkup(
                     [[
-                        InlineKeyboardButton("🤖 Join Our Bot Channel", url=f"https://t.me/{Var.UPDATES_CHANNEL}")
+                        InlineKeyboardButton("🤖 Jᴏɪɴ Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ", url=f"https://t.me/{Var.UPDATES_CHANNEL}")
                         ]]
                 ),
-                parse_mode=ParseMode.HTML
+                parse_mode="markdown"
             )
             return
         except Exception:
             await bot.send_message(
                 chat_id=message.chat.id,
-                text="<b>Something Wrong❗\nYou're Not Added Admin to Update Channel.\n\n👥 Support :- <a href=https://t.me/Star_Bots_Tamil_Support><b>Star Bots Tamil Support</b></a></b>",
-                parse_mode=ParseMode.HTML,
-                disable_web_page_preview=True,
-                quote=True)
+                text="__Sᴏᴍᴇᴛʜɪɴɢ ᴡᴇɴᴛ Wʀᴏɴɢ. Cᴏɴᴛᴀᴄᴛ ᴍᴇ__ [GreyMatter Support](https://t.me/greymatters_bots_discussion).",
+                parse_mode="markdown",
+                disable_web_page_preview=True)
             return
     await message.reply_text(
         text=HELP_TEXT,
-        parse_mode=ParseMode.HTML,
+        parse_mode="HTML",
         disable_web_page_preview=True,
-        reply_markup=HELP_BUTTONS,
-        quote=True
+        reply_markup=HELP_BUTTONS
         )
 
-BITLY_API = os.environ.get("BITLY_API", "aa2132168583d283fb288625d9352f2c5835512a")
-CUTTLY_API = os.environ.get("CUTTLY_API", "bd3a3ab946d7598ee459331dac9e9568e3d66")
-EZ4SHORT_API = os.environ.get("EZ4SHORT_API", "e41618d805b3c4256dfa99abde6ef11fc7629c47")
-TINYURL_API = os.environ.get("TINYURL_API", "iRkhyhlmfJ07cFVsFV0NpvX6dOWZIwPglbq8jQDuSBMqAEk5Y81BX04ejVQk")
-DROPLINK_API = os.environ.get("DROPLINK_API", "1d85e33efc4969b36e0f6c0a017aaaefd8accccc")
-TNSHORT_API = os.environ.get("TNSHORT_API", "d03a53149bf186ac74d58ff80d916f7a79ae5745")
 
-reply_markup = InlineKeyboardMarkup(
-        [[
-        InlineKeyboardButton('🎥 Movie Updates', url='https://t.me/Star_Moviess_Tamil')
-        ],
-        [
-        InlineKeyboardButton('🤖 Bot Channel', url=f"https://t.me/Star_Bots_Tamil"),
-        ],
-        [
-        InlineKeyboardButton('⚡ Request', user_id=OWNER_ID),
-        ],
-        [
-        InlineKeyboardButton('🚫 Close', callback_data='close_data')
-        ]]
-    )
 
-@Client.on_message(filters.command(["short"]) & filters.regex(r'https?://[^\s]+'))
-async def reply_shortens(bot, update):
-    message = await update.reply_text(
-        text="**Analysing Your Link...**",
-        disable_web_page_preview=True,
-        reply_markup=reply_markup,
-        quote=True
-    )
-    link = update.matches[0].group(0)
-    shorten_urls = await short(link)
-    await message.edit_text(
-        text=shorten_urls,
-        disable_web_page_preview=True,
-        reply_markup=reply_markup
-    )
-
-@Client.on_inline_query(filters.regex(r'https?://[^\s]+'))
-async def inline_short(bot, update):
-    link = update.matches[0].group(0),
-    shorten_urls = await short(link)
-    answers = [
-        InlineQueryResultArticle(
-            title="Short Links",
-            description=update.query,
-            input_message_content=InputTextMessageContent(
-                message_text=shorten_urls,
-                disable_web_page_preview=True
-            ),
-            reply_to_message_id=message.id
-        )
-    ]
-    await bot.answer_inline_query(
-        inline_query_id=update.id,
-        results=answers
-    )
-
-async def short(link):
-    shorten_urls = "**--Shortened URLs--**\n"
-    
-    # Bit.ly Shortener
-    if BITLY_API:
-        try:
-            s = Shortener(api_key=BITLY_API)
-            url = s.bitly.short(link)
-            shorten_urls += f"\n**Bit.ly :- {url}**\n"
-        except Exception as error:
-            print(f"Bit.ly Error :- {error}")
-        
-    # Clck.ru Shortener
-    try:
-        s = Shortener()
-        url = s.clckru.short(link)
-        shorten_urls += f"\n**Clck.ru :- {url}**\n"
-    except Exception as error:
-        print(f"Click.ru Error :- {error}")
-    
-    # Cutt.ly Shortener
-    if CUTTLY_API:
-        try:
-            s = Shortener(api_key=CUTTLY_API)
-            url = s.cuttly.short(link)
-            shorten_urls += f"\n**Cutt.ly :- {url}**\n"
-        except Exception as error:
-            print(f"Cutt.ly Error :- {error}")
-    
-    # Da.gd Shortener
-    try:
-        s = Shortener()
-        url = s.dagd.short(link)
-        shorten_urls += f"\n**Da.gd :- {url}**\n"
-    except Exception as error:
-        print(f"Da.gd Error :- {error}")
-    
-    # Is.gd Shortener
-    try:
-        s = Shortener()
-        url = s.isgd.short(link)
-        shorten_urls += f"\n**Is.gd :- {url}**\n"
-    except Exception as error:
-        print(f"Is.gd Error :- {error}")
-    
-    # Osdb.link Shortener
-    try:
-        s = Shortener()
-        url = s.osdb.short(link)
-        shorten_urls += f"\n**Osdb.link :- {url}**\n"
-    except Exception as error:
-        print(f"Osdb.link Error :- {error}")
-                
-    # Droplink.co Shortener
-    try:
-        api_url = "https://droplink.co/api" 
-        params = {'api': DROPLINK_API, 'url': link}
-        async with aiohttp.ClientSession() as session:
-            async with session.get(api_url, params=params, raise_for_status=True) as response:
-                data = await response.json()
-                url = data["shortenedUrl"]
-                shorten_urls += f"\n**DropLink.co :- {url}**\n"
-    except Exception as error:
-        print(f"Droplink.co Error :- {error}")
-
-    # TNShort.net Shortener
-    try:
-        api_url = "https://tnshort.net/api" 
-        params = {'api': TNSHORT_API, 'url': link}
-        async with aiohttp.ClientSession() as session:
-            async with session.get(api_url, params=params, raise_for_status=True) as response:
-                data = await response.json()
-                url = data["shortenedUrl"]
-                shorten_urls += f"\n**TNShort.net :- {url}**\n"
-    except Exception as error:
-        print(f"TNShort.net Error :- {error}")
-
-    # TinyURL.com Shortener
-    try:
-        s = Shortener(api_key=TINYURL_API)
-        url = s.tinyurl.short(link)
-        shorten_urls += f"\n**TinyURL.com :- {url}**\n"
-    except Exception as error:
-        print(f"TinyURL.com Error :- {error}")
-    
-    # Ez4short.com Shortener
-    try:
-        api_url = "https://ez4short.com/api" 
-        params = {'api': EZ4SHORT_API, 'url': link}
-        async with aiohttp.ClientSession() as session:
-            async with session.get(api_url, params=params, raise_for_status=True) as response:
-                data = await response.json()
-                url = data["shortenedUrl"]
-                shorten_urls += f"\n**Ez4short.com :- {url}**\n"
-    except Exception as error:
-        print(f"Ez4short.com Error :- {error}")
-
-   
-    # Send the text
-    try:
-        shorten_urls += ""
-        return shorten_urls
-    except Exception as error:
-        return error
