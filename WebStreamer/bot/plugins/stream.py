c9bb20b0a0436798e39014f53e09ba5bec5cf60f
HMTD-Links <109021412+HMTD-Links@users.noreply.github.com>
2023-05-26 18:11:34 +0530

    Update stream.py

--- WebStreamer/bot/plugins/stream.py
+++ WebStreamer/bot/plugins/stream.py
@@ -91,8 +91,19 @@ async def private_receive_handler(c: Client, m: Message):
     try:
         log_msg = await m.forward(chat_id=Var.BIN_CHANNEL)
         file_name = get_media_file_name(m)
-        stream_link = "https://{}:{}/{}/{}".format(Var.FQDN, Var.PORT, log_msg.id, file_name)
-        watch_link = "https://{}:{}/Watch/{}/{}".format(Var.FQDN, Var.PORT, log_msg.id, file_name)
+        file_hash = get_hash(log_msg, Var.HASH_LENGTH)
+        stream_link = "https://{}/{}/{}?hash={}".format(Var.FQDN, log_msg.id, file_name, file_hash) if Var.ON_HEROKU or Var.NO_PORT else \
+            "http://{}:{}/{}/{}?hash={}".format(Var.FQDN,
+                                    Var.PORT,
+                                    log_msg.id,
+                                    file_name,
+                                    file_hash)
+        watch_link = "https://{}/Watch/{}/{}?hash={}".format(Var.FQDN, log_msg.id, file_name, file_hash) if Var.ON_HEROKU or Var.NO_PORT else \
+            "http://{}:{}/Watch/{}/{}?hash={}".format(Var.FQDN,
+                                    Var.PORT,
+                                    log_msg.id,
+                                    file_name,
+                                    file_hash)
         file_hash = get_hash(log_msg, Var.HASH_LENGTH)
         file_name = get_name(log_msg)
         file_size = humanbytes(get_media_file_size(m))
@@ -138,11 +149,17 @@ async def channel_receive_handler(bot, broadcast):
     try:
         log_msg = await broadcast.forward(chat_id=Var.BIN_CHANNEL)
         file_name = get_media_file_name(broadcast)
-        stream_link = "https://{}:{}/{}".format(Var.FQDN, Var.PORT, log_msg.id, file_name)
+        file_hash = get_hash(log_msg, Var.HASH_LENGTH)
+        stream_link = "https://{}/{}/{}?hash={}".format(Var.FQDN, log_msg.id, file_name, file_hash) if Var.ON_HEROKU or Var.NO_PORT else \
+            "http://{}:{}/{}/{}?hash={}".format(Var.FQDN,
+                                    Var.PORT,
+                                    log_msg.id,
+                                    file_name,
+                                    file_hash)
         shortened_link = await get_shortlink(stream_link)
 
         await log_msg.reply_text(
-            text=f"<b>Channel Name :- <code>{broadcast.chat.title}</code>\nChannel ID :- <code>{broadcast.chat.id}</code>\nRequest URL :- https://t.me/{(await bot.get_me()).username}?start=Star_Bots_Tamil_{str(log_msg.id)}</b>",
+            text=f"<b>Channel Name :- {broadcast.chat.title}\nChannel ID :- <code>{broadcast.chat.id}</code>\nRequest URL :- https://t.me/{(await bot.get_me()).username}?start=Star_Bots_Tamil_{str(log_msg.id)}</b>",
             quote=True,
             parse_mode=ParseMode.HTML
         )
