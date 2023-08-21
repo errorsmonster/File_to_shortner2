fa39b7ea252759fbcf9c396e8541a8cc0a5caec6
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2023-08-21 07:07:22 +0530

    Update clients.py

--- WebStreamer/bot/clients.py
+++ WebStreamer/bot/clients.py
@@ -5,6 +5,7 @@ import asyncio
 import logging
 from os import environ
 from ..vars import Var
+from WebStreamer.utils.config_parser import TokenParser
 from pyrogram import Client
 from . import multi_clients, work_loads, sessions_dir, StreamBot
 
@@ -13,14 +14,7 @@ logger = logging.getLogger("multi_client")
 async def initialize_clients():
     multi_clients[0] = StreamBot
     work_loads[0] = 0
-    all_tokens = dict(
-        (c + 1, t)
-        for c, (_, t) in enumerate(
-            filter(
-                lambda n: n[0].startswith("MULTI_TOKEN"), sorted(environ.items())
-            )
-        )
-    )
+    all_tokens = TokenParser().parse_from_env()
     if not all_tokens:
         logger.info("No additional clients found, using default client")
         return
