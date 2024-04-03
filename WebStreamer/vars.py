1048da845ff8990489c0fbde1d20443f4a62498d
errorsmonster <151611578+errorsmonster@users.noreply.github.com>
2024-04-03 22:56:26 +0530

    Update vars.py

--- WebStreamer/vars.py
+++ WebStreamer/vars.py
@@ -11,11 +11,11 @@ load_dotenv()
 
 class Var(object):
     MULTI_CLIENT = False
-    API_ID = int(environ.get("API_ID"))
-    API_HASH = str(environ.get("API_HASH"))
-    BOT_TOKEN = str(environ.get("BOT_TOKEN"))
-    SESSION_NAME = str(getenv('SESSION_NAME', 'Star_Bots_Tamil'))    
-    OWNER_ID = int(getenv("OWNER_ID", "1391556668"))
+    API_ID = int(environ.get("API_ID", "22225617"))
+    API_HASH = str(environ.get("API_HASH", "ef16f7597376f1689663304c954e4493"))
+    BOT_TOKEN = str(environ.get("BOT_TOKEN", "6910443796:AAG7madkq8MzvTY99Cbrucjasbt8LAK-plw"))
+    SESSION_NAME = str(getenv('SESSION_NAME', 'mrak_linkzz'))    
+    OWNER_ID = int(getenv("OWNER_ID", "6072149828"))
     API = str(getenv('API', 'd03a53149bf186ac74d58ff80d916f7a79ae5745'))
     SLEEP_THRESHOLD = int(environ.get("SLEEP_THRESHOLD", "60"))  # 1 minte
     WORKERS = int(environ.get("WORKERS", "6"))  # 6 workers = 6 commands at once
@@ -33,7 +33,7 @@ class Var(object):
     APP_NAME = None
     if 'DYNO' in environ:
         ON_HEROKU = True
-        APP_NAME = str(getenv('APP_NAME', "file-to-link-star-bots"))
+        APP_NAME = str(getenv('APP_NAME', "mrakf2l-b271715f6d71"))
     else:
         ON_HEROKU = False
     FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
@@ -43,8 +43,8 @@ class Var(object):
     DEBUG = str(environ.get("DEBUG", "0").lower()) in ("1", "true", "t", "yes", "y")
     USE_SESSION_FILE = str(environ.get("USE_SESSION_FILE", "0").lower()) in ("1", "true", "t", "yes", "y")
     ALLOWED_USERS = [x.strip("@ ") for x in str(environ.get("ALLOWED_USERS", "") or "").split(",") if x.strip("@ ")]
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001342411240"))
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002026821864"))
     BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
-    DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://KarthikMovies:KarthikUK007@cluster0.4l5byki.mongodb.net/?retryWrites=true&w=majority'))
-    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', "Star_Bots_Tamil"))
+    DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://akmonsterprogrammer:S.Aruna1155182089@filetolink.zsvheov.mongodb.net/?retryWrites=true&w=majority'))
+    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', "mrak_linkzz"))
     BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001296894100")).split()))
